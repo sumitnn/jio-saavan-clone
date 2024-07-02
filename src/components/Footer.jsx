c2e9838328b6450f0ce492b901f85e628bb3e357
sumitnautiyal001 <sumit.nautiyal@ufaber.com>
2024-07-02 17:51:09 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,23 +7,22 @@ import { CiVolumeHigh } from "react-icons/ci";
 const Footer = () => {
   return (
     <>
-      <footer className='fixed bottom-0 left-0 right-0 border-t-2  mx-auto bg-[#F6F6F6] border-[#E9E9E9]'>
-        <div className=' md:mx-4 p-4 flex flex-row  justify-around md:justify-between items-center '>
-          <div className=''>
-            <img
-              src='https://placehold.co/600x400'
-              className='w-[80px] h-auto rounded-2xl'
-            />
+      <footer className='fixed bottom-0 left-0 right-0 border-t-2  bg-[#F6F6F6] border-[#E9E9E9] '>
+        <div className=' md:mx-4 p-4 flex flex-row  justify-between md:justify-between items-center '>
+          <img
+            src='https://placehold.co/600x400'
+            className='w-[80px] h-auto rounded-2xl'
+          />
+
+          <div className='w-72 flex gap-7 text-3xl text-[#9A9A9A] items-center'>
+            <FaRepeat  className=' cursor-pointer'/>
+            <GrChapterPrevious  className=' cursor-pointer'/>
+            <FaCirclePlay className=' cursor-pointer' />
+            <GrChapterNext  className=' cursor-pointer'/>
           </div>
-          <div className='flex gap-5 text-3xl text-[#9A9A9A]'>
-            <FaRepeat />
-            <GrChapterPrevious />
-            <FaCirclePlay />
-            <GrChapterNext />
-          </div>
-          <div className='text-3xl text-[#9A9A9A]'>
-            <CiVolumeHigh />
-          </div>
+          
+            <CiVolumeHigh className='text-3xl text-[#9A9A9A] cursor-pointer' />
+          
         </div>
       </footer>
     </>
